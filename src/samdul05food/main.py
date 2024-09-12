38bc7e4f4a8395f1ee63b69b103c92274d974ab1
GITSangWoo <dogcenta@gmail.com>
2024-09-12 19:54:20 +0900

    food csv compl

--- src/samdul05food/main.py
+++ src/samdul05food/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from typing import Union
+from datetime import datetime 
+import os 
+import pandas as pd
 
 app = FastAPI()
 
@@ -10,6 +13,26 @@ def read_root():
 @app.get("/food")
 def food(name:str):
     # 시간을 구함
+    time = datetime.now()
+    time = time.strftime('%Y-%m-%d %H:%M:%S')
     # 음식 이름과 시간을 csv로 저장 -> /code/data/food.csv
-    return {"food":name, "time":"2024-09-15 11:12:13"}
+    home_path = os.path.expanduser('~')
+    file_path=f"{home_path}/data/foodapp/data.csv"
+    data_path=f"{home_path}/data/foodapp/"
+    if os.path.exists(file_path):
+        df=pd.read_csv(file_path)     
+    else :
+        df=pd.DataFrame(
+            {
+            'food' : [],
+            'time': [],
+            }
+        )
+    df.loc[len(df)] = [name,time]
+    os.makedirs(os.path.dirname(data_path), exist_ok = True)
+    df.to_csv(file_path,index=False)
+    
+    
+    return {"food":name, "time": time }
+
 
